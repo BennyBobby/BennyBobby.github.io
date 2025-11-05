c34afbe79fff8aab9b8343dc1c025c6bd9ab1251
Benny L <benny.lay2003@gmail.com>
2025-11-06 00:07:44 +0100

    Adding project and contact section

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 function toggleDropdown() {
-    document.getElementById("projetsDropdown").classList.toggle("show");
+  document.getElementById("projetsDropdown").classList.toggle("show");
 }
 
-
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        for (var i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
     }
-}
+  }
+};
